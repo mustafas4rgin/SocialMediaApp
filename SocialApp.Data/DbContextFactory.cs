c1d5555626a0a9857d8c512ce74c35d61cb8e7dc
Mustafa Sargın <mustafas4rgin@gmail.com>
2025-10-17 02:23:51 +0300

    dev-db

--- SocialApp.Data/DbContextFactory.cs
+++ SocialApp.Data/DbContextFactory.cs
@@ -9,7 +9,7 @@ public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<AppDbConte
         public AppDbContext CreateDbContext(string[] args)
         {
             var optionsBuilder = new Microsoft.EntityFrameworkCore.DbContextOptionsBuilder<AppDbContext>();
-            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Username=postgres;Password=postgres;Database=SocialApp");
+            optionsBuilder.UseNpgsql("Host=dpg-d3onpbs9c44c738bpgng-a.oregon-postgres.render.com;Database=s4rgin;Username=s4rgin_user;Password=AuIbUXiPZ5ThC0T6EEw6o1xtrTzkAYTZ;Port=5432;Ssl Mode=Require;Trust Server Certificate=true");
 
             return new AppDbContext(optionsBuilder.Options);
         }
