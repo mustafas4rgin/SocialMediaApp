ecabc13ea03fbaefaf6b863b0a60469e4e77211f
Mustafa Can Sargın <165585760+mustafas4rgin@users.noreply.github.com>
2025-11-01 19:01:03 +0300

    follow-controller-update

--- SocialApp.API/Controllers/FollowController.cs
+++ SocialApp.API/Controllers/FollowController.cs
@@ -7,6 +7,7 @@ using SocialApp.Domain.DTOs.Create;
 using SocialApp.Domain.DTOs.List;
 using SocialApp.Domain.DTOs.Update;
 using SocialApp.Domain.Entities;
+using SocialApp.Domain.Parameters;
 
 namespace SocialApp.API.Controllers
 {
@@ -14,12 +15,59 @@ namespace SocialApp.API.Controllers
     [ApiController]
     public class FollowController : GenericController<Follow, FollowDTO, CreateFollowDTO, UpdateFollowDTO>
     {
+        private readonly IFollowService _followService;
+        private readonly IMapper _mapper;
         public FollowController(
         IValidator<CreateFollowDTO> createValidator,
         IValidator<UpdateFollowDTO> updateValidator,
         IFollowService followService,
         IMapper mapper
         ) : base(createValidator, updateValidator, followService, mapper)
-        {}
+        {
+            _followService = followService;
+            _mapper = mapper;
+        }
+        public override async Task<IActionResult> GetAllAsync([FromQuery] QueryParameters param, CancellationToken ct = default)
+        {
+            var result = await _followService.GetAllFollowsWithIncludesAsync(param, ct);
+
+            var errorResult = HandleServiceResult(result);
+
+            if (errorResult != null)
+                return errorResult;
+
+            var follows = result.Data;
+
+            var dto = _mapper.Map<IEnumerable<FollowDTO>>(follows);
+
+            return Ok(
+            new
+            {
+                result.Message,
+                Follows = dto
+            }
+            );
+        }
+        public override async Task<IActionResult> GetByIdAsync([FromRoute]int id, [FromQuery]QueryParameters param, CancellationToken ct = default)
+        {
+            var result = await _followService.GetFollowByIdWithIncludesAsync(id, param, ct);
+
+            var errorResult = HandleServiceResult(result);
+
+            if (errorResult != null)
+                return errorResult;
+
+            var follow = result.Data;
+
+            var dto = _mapper.Map<FollowDTO>(follow);
+
+            return Ok(
+            new
+            {
+                result.Message,
+                Follow = dto
+            }
+            );
+        }
     }
 }
