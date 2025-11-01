c5db410f7a575199caeb574487b34a12cf4a2263
Mustafa Can Sargın <165585760+mustafas4rgin@users.noreply.github.com>
2025-11-01 19:22:10 +0300

    user-controller-update

--- SocialApp.API/Controllers/UserController.cs
+++ SocialApp.API/Controllers/UserController.cs
@@ -7,6 +7,7 @@ using SocialApp.Domain.DTOs.Create;
 using SocialApp.Domain.DTOs.List;
 using SocialApp.Domain.DTOs.Update;
 using SocialApp.Domain.Entities;
+using SocialApp.Domain.Parameters;
 
 namespace SocialApp.API.Controllers
 {
@@ -14,12 +15,59 @@ namespace SocialApp.API.Controllers
     [ApiController]
     public class UserController : GenericController<User, UserDTO, CreateUserDTO, UpdateUserDTO>
     {
+        private readonly IUserService _userService;
+        private readonly IMapper _mapper;
         public UserController(
         IValidator<CreateUserDTO> createValidator,
         IValidator<UpdateUserDTO> updateValidator,
         IUserService userService,
         IMapper mapper
         ) : base(createValidator, updateValidator, userService, mapper)
-        {}
+        {
+            _mapper = mapper;
+            _userService = userService;
+        }
+        public override async Task<IActionResult> GetAllAsync([FromQuery] QueryParameters param, CancellationToken ct = default)
+        {
+            var result = await _userService.GetAllUsersWithIncludesAsync(param, ct);
+
+            var errorResult = HandleServiceResult(result);
+
+            if (errorResult != null)
+                return errorResult;
+
+            var users = result.Data;
+
+            var dto = _mapper.Map<IEnumerable<UserDTO>>(users);
+
+            return Ok(
+            new
+            {
+                result.Message,
+                User = dto
+            }
+            );
+        }
+        public override async Task<IActionResult> GetByIdAsync([FromRoute]int id, [FromQuery]QueryParameters param, CancellationToken ct = default)
+        {
+            var result = await _userService.GetUserByIdWithIncludesAsync(id, param, ct);
+
+            var errorResult = HandleServiceResult(result);
+
+            if (errorResult != null)
+                return errorResult;
+
+            var user = result.Data;
+
+            var dto = _mapper.Map<UserDTO>(user);
+
+            return Ok(
+            new
+            {
+                result.Message,
+                User = dto
+            }
+            );
+        }
     }
 }
